7ab27f48a263c9a9833058d082a0d5c83df73945
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2025-01-10 14:11:49 +0100

    Version final Orientado a Objetos

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+group = "srangeldev"
+version = "BETA 1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    //logger
+    //implementation("org.lighthousegames:logging.1.5.0")
+    implementation("ch.qos.logback:logback-classic:1.5.16")
+
+    //mordant
+    implementation("com.github.ajalt.mordant:mordant:3.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    archiveFileName.set("Howarts.jar")
+}
+
